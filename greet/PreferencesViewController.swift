57528b2e4bf991fbead1b3b26e6a34aca5f3f60c
Tyler Geery <tyler.geery@yahoo.com>
2015-03-17 01:02:57 -0700

    marginal improvements

--- greet/PreferencesViewController.swift
+++ greet/PreferencesViewController.swift
@@ -13,9 +13,19 @@ class PreferencesViewController: UIViewController {
     @IBOutlet weak var socialButton: UIButton!
     @IBOutlet weak var locationButton: UIButton!
 
+    @IBOutlet weak var socialView: UIView!
+    @IBOutlet weak var locationView: UIView!
+
+    @IBOutlet weak var barPreferenceView: UIView!
+    @IBOutlet weak var barPreferenceButton: UIButton!
+
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var locationSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationView.hidden = true;
 
         socialButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Selected)
         locationButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Selected)
@@ -37,9 +47,11 @@ class PreferencesViewController: UIViewController {
 
     @IBAction func setButtonSocialState(sender: AnyObject) {
         setButtonState("default")
+        setPreferencesView("social")
     }
     @IBAction func setButtonLocationState(sender: AnyObject) {
         setButtonState("location")
+        setPreferencesView("location")
     }
     func setButtonState(selected: String) {
         var active: UIButton
@@ -61,5 +73,28 @@ class PreferencesViewController: UIViewController {
 
     }
 
+    @IBAction func barPreferenceButtonPressed(sender: AnyObject) {
+        let startingPosition = self.barPreferenceButton.frame.origin.x
+        let offset = (startingPosition > 0) ? 0 : self.barPreferenceView.frame.size.width - self.barPreferenceButton.frame.size.width
+        println("view width: \(startingPosition), offset: \(offset)")
+        UIView.animateWithDuration(1, animations: {
+            self.barPreferenceButton.frame.origin = CGPoint(x: offset, y: self.barPreferenceButton.frame.origin.y)
+            }, completion: { (Value: Bool) in
+                println("completed now do something else")
+        })
+    }
+    @IBAction func locationPreferenceChanged(sender: AnyObject) {
+        let dist = String(Int(self.locationSlider.value))
+        self.locationLabel.text = (dist == "50") ? "50+ mi" : dist + " mi"
+    }
+    func setPreferencesView(selected: String) {
+        if selected == "location" {
+            self.locationView.hidden = false
+            self.socialView.hidden = true
+        } else {
+            self.locationView.hidden = true
+            self.socialView.hidden = false
+        }
+    }
 }
 
